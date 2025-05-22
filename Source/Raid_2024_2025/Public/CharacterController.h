a35cb295489e020dbf23b8b4df3ee5d6e0f2b861
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2025-05-22 16:34:00 +0200

    CharacterController V0.7

--- Source/Raid_2024_2025/Public/CharacterController.h
+++ Source/Raid_2024_2025/Public/CharacterController.h
@@ -18,9 +18,19 @@ public:
     UPROPERTY(EditAnywhere, Category = "Movement")
     float DashForce = 6.f;
 
+    UPROPERTY(EditAnywhere, Category = "Movement")
+    float DashFriction = 4.f;
+
+    UPROPERTY(EditAnywhere, Category = "Movement")
+    bool CanDash = true;
+
 protected:
     virtual void BeginPlay() override;
+    virtual void Landed(const FHitResult& Hit) override;
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    UPROPERTY(EditAnywhere, Category = "Movement")
+    bool DashAir = false;
 
     UFUNCTION(BlueprintCallable, Category = "Movement")
     void Move(float ActionValue);
