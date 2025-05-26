0dcf5d75efa6b3e47a356d86cdbb65543bb22aa6
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2025-05-26 10:33:30 +0200

    Move left and right

---
+++ Source/Raid_2024_2025/Private/3C/CharacterPawnMovementComponent.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "3C/CharacterPawnMovementComponent.h"
+
+#include "Components/CapsuleComponent.h"
+#include "Math/UnitConversion.h"
+
+
+// Sets default values for this component's properties
+UCharacterPawnMovementComponent::UCharacterPawnMovementComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UCharacterPawnMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CollisionParams = FCollisionQueryParams(FName (TEXT("FeetTrace")), false, GetOwner());
+	if (UCapsuleComponent* Capsule = Cast<UCapsuleComponent>(UpdatedComponent))
+	{
+		FeetShape = FCollisionShape::MakeSphere(Capsule->GetScaledCapsuleRadius() - FeetSkin);
+	}
+	
+}
+
+
+// Called every frame
+void UCharacterPawnMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                    FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!CurrentDirection.IsNearlyZero())
+	{
+		WalkProgress += DeltaTime / AccelTime;
+		FMath::Clamp(WalkProgress, 0,1);
+		
+		CurrentSpeed = SpeedCurve->GetFloatValue(WalkProgress) * VMax;
+
+		FVector2D MoveVector2 = CurrentSpeed * DeltaTime * CurrentDirection;
+		FVector MoveVector = FVector(MoveVector2.X, MoveVector2.Y, 0);
+
+		UpdatedComponent->MoveComponent(MoveVector, FQuat::Identity, true);
+	}
+}
+
+void UCharacterPawnMovementComponent::JumpInput()
+{
+	
+}
+
+void UCharacterPawnMovementComponent::DashInput()
+{
+	
+}	
+
+void UCharacterPawnMovementComponent::MoveInput(const FVector2D& Direction)
+{
+	CurrentDirection = Direction.GetSafeNormal();
+}
+
+float UCharacterPawnMovementComponent::EaseOutQuart(float x)
+{
+	return 1 - pow(1-x, 4);
+}
+
---
+++ Source/Raid_2024_2025/Public/3C/CharacterPawn.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "GameFramework/Pawn.h"
+#include "CharacterPawn.generated.h"
+
+
+class UCharacterPawnMovementComponent;
+
+UCLASS()
+class RAID_2024_2025_API ACharacterPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACharacterPawn();
+
+protected:
+#pragma region Input Config
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<class UInputConfig> InputConfig;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+#pragma endregion 
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void NotifyControllerChanged() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<class APlayerController> _controller;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Move(const FInputActionValue& InputActionValue);
+	virtual void Jump();
+	virtual void Dash();
+	virtual void MoveCancelled();
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UCharacterPawnMovementComponent> MovementComponent;
+
+};
