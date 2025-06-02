795146eb38152f349105042f085e78fc87add21a
Elckarow <101005497+Elckarow@users.noreply.github.com>
2025-06-02 14:47:59 +0200

    create SavableActor

---
+++ Source/Raid_2024_2025/Private/SaveSystem/SavableActor.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SaveSystem/SavableActor.h"
+
+ALWAYS_RECREATE(ASavableActor)
+
+// Sets default values
+ASavableActor::ASavableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASavableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASavableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
