275a5b189886efeeaa713b25f8bd06a10583ce23
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2025-06-02 15:23:04 +0200

    Dash + gravity changes (now physics accurate)

--- Source/Raid_2024_2025/Public/3C/CharacterPawnMovementComponent.h
+++ Source/Raid_2024_2025/Public/3C/CharacterPawnMovementComponent.h
@@ -4,83 +4,88 @@
 #include "GameFramework/PawnMovementComponent.h"
 #include "CharacterPawnMovementComponent.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class RAID_2024_2025_API UCharacterPawnMovementComponent : public UPawnMovementComponent
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	UCharacterPawnMovementComponent();
+    UCharacterPawnMovementComponent();
 
-	void JumpInput();
-	void DashInput();
-	void MoveInput(const FVector2D& Direction);
+    void JumpInput();
+    void DashInput();
+    void MoveInput(const FVector2D& Direction);
 
-	float CurrentSpeed = 0;
+    float CurrentSpeed = 0;
 
 protected:
-	virtual void BeginPlay() override;
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    virtual void BeginPlay() override;
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	void PerformSlideAsyncTrace(const FVector& DesiredMove);
-	void OnAsyncTraceResult(const FTraceHandle& Handle, FTraceDatum& Datum);
+    void PerformSlideAsyncTrace();
+    void OnAsyncTraceResult(const FTraceHandle& Handle, FTraceDatum& Datum);
+    bool CheckIfGrounded();
 
-	UPROPERTY(EditDefaultsOnly, Category="Acceleration")
-	TObjectPtr<UCurveFloat> SpeedCurve;
+    // Courbes
+    UPROPERTY(EditDefaultsOnly, Category = "Movement|Curves")
+    TObjectPtr<UCurveFloat> SpeedCurve;
 
-	UPROPERTY(EditDefaultsOnly, Category="Acceleration")
-	float VMax = 100;
+    UPROPERTY(EditDefaultsOnly, Category = "Movement|Curves")
+    TObjectPtr<UCurveFloat> DashCurve;
 
-	UPROPERTY(EditDefaultsOnly, Category="Acceleration")
-	float AccelTime = 0.5f;
+    // Mouvement de base
+    UPROPERTY(EditDefaultsOnly, Category = "Movement")
+    float VMax = 100.f;
 
-	UPROPERTY(EditDefaultsOnly)
-	float FeetSkin = 1;
+    UPROPERTY(EditDefaultsOnly, Category = "Movement")
+    float AccelTime = 0.5f;
 
-#pragma region Slope Slide
-	UPROPERTY(EditDefaultsOnly, Category = "Movement|Slope")
-	float MaxGroundAngle = 45.f;
+    // Masse / physique
+    UPROPERTY(EditDefaultsOnly, Category = "Physics")
+    float Mass = 70.f; // en kg
 
-	UPROPERTY(EditDefaultsOnly, Category = "Movement|Slope")
-	float SlopeSlideSpeed = 600.f;
-#pragma endregion
+    UPROPERTY(EditDefaultsOnly, Category = "Physics")
+    float Gravity = -980.f; // cm/s²
+
+    // Saut
+    UPROPERTY(EditDefaultsOnly, Category = "Jump")
+    float JumpVelocity = 600.f;
+
+    bool bIsJumping = false;
+    float VerticalSpeed = 0.f;
+    bool bIsGrounded = true;
+
+    // Dash
+    UPROPERTY(EditDefaultsOnly, Category = "Dash")
+    float DashDuration = 0.3f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Dash")
+    float DashCooldown = 1.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Dash")
+    float DashDistance = 1000.f;
+
+    float DashTimer = 0.f;
+    float DashCooldownTimer = 0.f;
+    FVector2D DashDirection;
+    bool bIsDashing = false;
+
+    // Sol
+    UPROPERTY(EditDefaultsOnly, Category = "Feet Trace")
+    float FeetSkin = 1.f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Feet Trace")
+    float MaxGroundAngle = 45.f;
 
 private:
-	FVector2D CurrentDirection;
-	float WalkProgress = 0;
-	bool bCanMove = true;
-	bool CheckIfGrounded();
+    FVector2D CurrentDirection;
+    float WalkProgress = 0.f;
+    bool bCanMove = true;
 
-#pragma region Dash
-	UPROPERTY(EditDefaultsOnly, Category="Movement|Dash")
-	float DashTime = 0.15f;
-
-	UPROPERTY(EditDefaultsOnly, Category="Movement|Dash")
-	float DashSpeed = 2500.0f;
-
-	UPROPERTY(EditDefaultsOnly, Category="Movement|Dash")
-	float DashCooldown = 1.0f;
-
-	bool bIsDashing = false;
-	float DashTimer = 0.0f;
-	FVector2D DashDirection;
-	float DashCooldownTimer = 0.0f;
-#pragma endregion
-
-#pragma region Feet Trace
-	float halfHeight = 0.0f;
-	float CapsuleStep = 0.0f;
-	float StepMult = 0.0f;
-	FCollisionQueryParams CollisionParams;
-	FTraceDelegate TraceDelegate;
-	FCollisionShape FeetShape;
-#pragma endregion
-
-#pragma region Jump
-	bool bIsJumping = false;
-	float JumpVelocity = 900.f;
-	float VerticalSpeed = 0.f;
-	float Gravity = -2000.f;
-	bool bIsGrounded = true;
-#pragma endregion
+    // Capsule
+    float halfHeight = 0.0f;
+    float CapsuleStep = 0.0f;
+    float StepMult = 0.0f;
+    FCollisionQueryParams CollisionParams;
+    FCollisionShape FeetShape;
 };
