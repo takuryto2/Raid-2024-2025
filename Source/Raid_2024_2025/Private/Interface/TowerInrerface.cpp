870573ac43d720015661c3c2d60793e5a9dfb413
takuryto2 <louisguichard761@gmail.com>
2025-06-05 10:24:32 +0200

    quick push

---
+++ Source/Raid_2024_2025/Private/Interface/TowerInrerface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/TowerInrerface.h"
+
+// Add default functionality here for any ITowerInrerface functions that are not pure virtual.
